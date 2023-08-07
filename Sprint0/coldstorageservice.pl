bc87dc5cb71b4e50cb084a4c1b54c687cb54f5dd
cronoimpius <gianmiriano.porrazzo@studio.unibo.it>
2023-08-07 17:20:37 +0200

    modifiche dai todo

--- Sprint0/coldstorageservice.pl
+++
@@ -1,8 +0,0 @@
-%====================================================================================
-% coldstorageservice description   
-%====================================================================================
-context(ctxledqak, "192.168.1.xxx",  "TCP", "8086").
-context(ctxstorageservice, "localhost",  "TCP", "8090").
- qactor( ledqakactor, ctxledqak, "it.unibo.ledqakactor.Ledqakactor").
-  qactor( coldstorageserviceactor, ctxstorageservice, "it.unibo.coldstorageserviceactor.Coldstorageserviceactor").
-  qactor( transporttrolley, ctxstorageservice, "it.unibo.transporttrolley.Transporttrolley").
