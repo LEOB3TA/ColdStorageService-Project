2e4c912018b7b280d4d6457bdf2524bbefd5aaeb
Leonardo Focardi <leonardo.foc.lav@gmail.com>
2023-08-08 11:36:25 +0200

    aggiunto codedQactor al qak compresi file kotlin necessari per il funzionamento

--- Sprint0/Arch_coldstorageservice.py
+++ Sprint0/Arch_coldstorageservice.py
@@ -19,6 +19,8 @@ eventedgeattr = {
 with Diagram('coldstorageserviceArch', show=False, outformat='png', graph_attr=graphattr) as diag:
   with Cluster('env'):
      sys = Custom('','./qakicons/system.png')
+     with Cluster('ctxsonarqak23', graph_attr=nodeattr):
+          sonar=Custom('sonar(coded)','./qakicons/codedQActor.png')
      with Cluster('ctxledqak', graph_attr=nodeattr):
           ledqakactor=Custom('ledqakactor','./qakicons/symActorSmall.png')
      with Cluster('ctxstorageservice', graph_attr=nodeattr):
--- Sprint0/coldstorageservice.pl
+++ Sprint0/coldstorageservice.pl
@@ -1,8 +1,10 @@
 %====================================================================================
 % coldstorageservice description   
 %====================================================================================
+context(ctxsonarqak23, "localhost",  "TCP", "8128").
 context(ctxledqak, "192.168.1.xxx",  "TCP", "8086").
 context(ctxstorageservice, "localhost",  "TCP", "8090").
- qactor( ledqakactor, ctxledqak, "it.unibo.ledqakactor.Ledqakactor").
+ qactor( sonar, ctxsonarqak23, "sonarSimulator").
+  qactor( ledqakactor, ctxledqak, "it.unibo.ledqakactor.Ledqakactor").
   qactor( coldstorageserviceactor, ctxstorageservice, "it.unibo.coldstorageserviceactor.Coldstorageserviceactor").
   qactor( transporttrolley, ctxstorageservice, "it.unibo.transporttrolley.Transporttrolley").
