a0bcf0ba466cc85a2ed10fe19655c7cef2e7af5a
cronoimpius <gianmiriano.porrazzo@studio.unibo.it>
2023-08-14 10:17:40 +0200

    piccoli cambiamenti e modifiche
    inserimento immagini, aggiunta service access gui

--- Sprint0/Arch_coldstorageservice.py
+++ Sprint0/Arch_coldstorageservice.py
@@ -26,19 +26,22 @@ with Diagram('coldstorageserviceArch', show=False, outformat='png', graph_attr=g
           controller23=Custom('controller23','./qakicons/symActorSmall.png')
           sonar23=Custom('sonar23(coded)','./qakicons/codedQActor.png')
      with Cluster('ctxstorageservice', graph_attr=nodeattr):
-          coldstorageserviceactor=Custom('coldstorageserviceactor','./qakicons/symActorSmall.png')
+          coldstorageservice=Custom('coldstorageservice','./qakicons/symActorSmall.png')
           transporttrolley=Custom('transporttrolley','./qakicons/symActorSmall.png')
+     with Cluster('ctxtruck', graph_attr=nodeattr):
+          serviceaccesgui=Custom('serviceaccesgui','./qakicons/symActorSmall.png')
      controller23 >> Edge(color='blue', style='solid', xlabel='sonaractivate', fontcolor='blue') >> sonar23
      sys >> Edge(color='red', style='dashed', xlabel='sonardata', fontcolor='red') >> controller23
      sys >> Edge(color='red', style='dashed', xlabel='robotmoving', fontcolor='red') >> controller23
      controller23 >> Edge(color='blue', style='solid', xlabel='ledCmd', fontcolor='blue') >> ledqakactor
-     coldstorageserviceactor >> Edge(color='magenta', style='solid', xlabel='pickup', fontcolor='magenta') >> transporttrolley
-     coldstorageserviceactor >> Edge(color='blue', style='solid', xlabel='dropout', fontcolor='blue') >> transporttrolley
-     coldstorageserviceactor >> Edge(color='blue', style='solid', xlabel='backhome', fontcolor='blue') >> transporttrolley
+     coldstorageservice >> Edge(color='magenta', style='solid', xlabel='pickup', fontcolor='magenta') >> transporttrolley
+     coldstorageservice >> Edge(color='blue', style='solid', xlabel='dropout', fontcolor='blue') >> transporttrolley
+     coldstorageservice >> Edge(color='blue', style='solid', xlabel='backhome', fontcolor='blue') >> transporttrolley
      transporttrolley >> Edge(color='magenta', style='solid', xlabel='step', fontcolor='magenta') >> basicrobot
      transporttrolley >> Edge(color='blue', style='solid', xlabel='cmd', fontcolor='blue') >> basicrobot
      transporttrolley >> Edge(color='blue', style='solid', xlabel='end', fontcolor='blue') >> basicrobot
      transporttrolley >> Edge(color='magenta', style='solid', xlabel='doplan', fontcolor='magenta') >> basicrobot
      transporttrolley >> Edge(color='magenta', style='solid', xlabel='engage', fontcolor='magenta') >> basicrobot
      transporttrolley >> Edge(color='blue', style='solid', xlabel='disengage', fontcolor='blue') >> basicrobot
+     serviceaccesgui >> Edge(color='magenta', style='solid', xlabel='storeFood', fontcolor='magenta') >> coldstorageservice
 diag
--- Sprint0/coldstorageservice.pl
+++ Sprint0/coldstorageservice.pl
@@ -4,9 +4,11 @@
 context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
 context(ctxrasp, "192.168.1.xxx",  "TCP", "8086").
 context(ctxstorageservice, "localhost",  "TCP", "8090").
+context(ctxtruck, "192.168.1.xxx",  "TCP", "8085").
  qactor( basicrobot, ctxbasicrobot, "external").
-  qactor( sonar23, ctxrasp, "sonarSimulator").
+  qactor( sonar23, ctxrasp, "rx.sonarSimulator").
   qactor( ledqakactor, ctxrasp, "it.unibo.ledqakactor.Ledqakactor").
   qactor( controller23, ctxrasp, "it.unibo.controller23.Controller23").
-  qactor( coldstorageserviceactor, ctxstorageservice, "it.unibo.coldstorageserviceactor.Coldstorageserviceactor").
+  qactor( coldstorageservice, ctxstorageservice, "it.unibo.coldstorageservice.Coldstorageservice").
   qactor( transporttrolley, ctxstorageservice, "it.unibo.transporttrolley.Transporttrolley").
+  qactor( serviceaccesgui, ctxtruck, "it.unibo.serviceaccesgui.Serviceaccesgui").
