16ad8a80f388e541a724590a9d6ac8e35f251a7a
Leonardo Focardi <leonardo.foc.lav@gmail.com>
2023-08-23 11:19:57 +0200

    qak generato

--- Sprint0Reviewed/Arch_coldstorageservice.py
+++ Sprint0Reviewed/Arch_coldstorageservice.py
@@ -19,26 +19,18 @@ eventedgeattr = {
 with Diagram('coldstorageserviceArch', show=False, outformat='png', graph_attr=graphattr) as diag:
   with Cluster('env'):
      sys = Custom('','./qakicons/system.png')
+     with Cluster('ctxstorageservice', graph_attr=nodeattr):
+          servicestatusgui=Custom('servicestatusgui','./qakicons/symActorSmall.png')
+          transporttrolley=Custom('transporttrolley','./qakicons/symActorSmall.png')
+          coldstorageservice=Custom('coldstorageservice','./qakicons/symActorSmall.png')
      with Cluster('ctxbasicrobot', graph_attr=nodeattr):
           basicrobot=Custom('basicrobot(ext)','./qakicons/externalQActor.png')
-     with Cluster('ctxrasp', graph_attr=nodeattr):
-          ledqakactor=Custom('ledqakactor','./qakicons/symActorSmall.png')
-          controller23=Custom('controller23','./qakicons/symActorSmall.png')
-          sonar23=Custom('sonar23(coded)','./qakicons/codedQActor.png')
-     with Cluster('ctxstorageservice', graph_attr=nodeattr):
-          coldstorageservice=Custom('coldstorageservice','./qakicons/symActorSmall.png')
-          transporttrolley=Custom('transporttrolley','./qakicons/symActorSmall.png')
      with Cluster('ctxtruck', graph_attr=nodeattr):
           serviceaccesgui=Custom('serviceaccesgui','./qakicons/symActorSmall.png')
-     with Cluster('ctxmanager', graph_attr=nodeattr):
-          servicestatusgui=Custom('servicestatusgui','./qakicons/symActorSmall.png')
-     controller23 >> Edge(color='blue', style='solid', xlabel='sonaractivate', fontcolor='blue') >> sonar23
-     sys >> Edge(color='red', style='dashed', xlabel='sonardata', fontcolor='red') >> controller23
-     sys >> Edge(color='red', style='dashed', xlabel='robotmoving', fontcolor='red') >> controller23
-     controller23 >> Edge(color='blue', style='solid', xlabel='ledCmd', fontcolor='blue') >> ledqakactor
-     coldstorageservice >> Edge(color='blue', style='solid', xlabel='updategui', fontcolor='blue') >> servicestatusgui
-     coldstorageservice >> Edge(color='magenta', style='solid', xlabel='pickup', fontcolor='magenta') >> transporttrolley
-     coldstorageservice >> Edge(color='blue', style='solid', xlabel='dropout', fontcolor='blue') >> transporttrolley
-     coldstorageservice >> Edge(color='blue', style='solid', xlabel='backhome', fontcolor='blue') >> transporttrolley
+     with Cluster('ctxrasp', graph_attr=nodeattr):
+          led=Custom('led','./qakicons/symActorSmall.png')
+          sonar23=Custom('sonar23(coded)','./qakicons/codedQActor.png')
      serviceaccesgui >> Edge(color='magenta', style='solid', xlabel='storeFood', fontcolor='magenta') >> coldstorageservice
+     serviceaccesgui >> Edge(color='magenta', style='solid', xlabel='deposit', fontcolor='magenta') >> coldstorageservice
+     coldstorageservice >> Edge(color='magenta', style='solid', xlabel='pickup', fontcolor='magenta') >> transporttrolley
 diag
--- Sprint0Reviewed/coldstorageservice.pl
+++ Sprint0Reviewed/coldstorageservice.pl
@@ -1,16 +1,14 @@
 %====================================================================================
 % coldstorageservice description   
 %====================================================================================
+context(ctxstorageservice, "localhost",  "TCP", "8092").
 context(ctxbasicrobot, "localhost",  "TCP", "8020").
-context(ctxrasp, "192.168.1.xxx",  "TCP", "8086").
-context(ctxstorageservice, "localhost",  "TCP", "8090").
 context(ctxtruck, "localhost",  "TCP", "8087").
-context(ctxmanager, "localhost",  "TCP", "8085").
+context(ctxrasp, "192.168.1.xxx",  "TCP", "8086").
  qactor( basicrobot, ctxbasicrobot, "external").
   qactor( sonar23, ctxrasp, "rx.sonarSimulator").
-  qactor( ledqakactor, ctxrasp, "it.unibo.ledqakactor.Ledqakactor").
-  qactor( controller23, ctxrasp, "it.unibo.controller23.Controller23").
-  qactor( coldstorageservice, ctxstorageservice, "it.unibo.coldstorageservice.Coldstorageservice").
-  qactor( transporttrolley, ctxstorageservice, "it.unibo.transporttrolley.Transporttrolley").
+  qactor( led, ctxrasp, "it.unibo.led.Led").
   qactor( serviceaccesgui, ctxtruck, "it.unibo.serviceaccesgui.Serviceaccesgui").
-  qactor( servicestatusgui, ctxmanager, "it.unibo.servicestatusgui.Servicestatusgui").
+  qactor( servicestatusgui, ctxstorageservice, "it.unibo.servicestatusgui.Servicestatusgui").
+  qactor( transporttrolley, ctxstorageservice, "it.unibo.transporttrolley.Transporttrolley").
+  qactor( coldstorageservice, ctxstorageservice, "it.unibo.coldstorageservice.Coldstorageservice").
