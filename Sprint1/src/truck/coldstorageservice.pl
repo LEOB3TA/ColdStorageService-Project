d12603ee6f5b558f46ba2f52d900c4b07fdf8749
leo <leonardo.foc.lav@gmail.com>
2023-08-24 16:50:25 +0200

    test da rivedere

---
+++ Sprint1/src/truck/coldstorageservice.pl
@@ -0,0 +1,6 @@
+%====================================================================================
+% coldstorageservice description   
+%====================================================================================
+context(ctxtruck, "localhost",  "TCP", "8092").
+ qactor( coldstorageservice, ctxtruck, "it.unibo.coldstorageservice.Coldstorageservice").
+  qactor( mocktruck, ctxtruck, "it.unibo.mocktruck.Mocktruck").
