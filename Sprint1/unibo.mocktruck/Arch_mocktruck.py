8c5da55c02cb1e25ac7731bce7e6675455920e1d
Leonardo Focardi <leonardo.foc.lav@gmail.com>
2023-08-26 14:11:58 +0200

    fix to project

---
+++ Sprint1/unibo.mocktruck/Arch_mocktruck.py
@@ -0,0 +1,28 @@
+from diagrams import Cluster, Diagram, Edge
+from diagrams.custom import Custom
+import os
+os.environ['PATH'] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
+
+graphattr = {     #https://www.graphviz.org/doc/info/attrs.html
+    'fontsize': '22',
+}
+
+nodeattr = {   
+    'fontsize': '22',
+    'bgcolor': 'lightyellow'
+}
+
+eventedgeattr = {
+    'color': 'red',
+    'style': 'dotted'
+}
+with Diagram('mocktruckArch', show=False, outformat='png', graph_attr=graphattr) as diag:
+  with Cluster('env'):
+     sys = Custom('','./qakicons/system.png')
+     with Cluster('ctxtruck', graph_attr=nodeattr):
+          coldstorageservice=Custom('coldstorageservice','./qakicons/symActorSmall.png')
+          mocktruck=Custom('mocktruck','./qakicons/symActorSmall.png')
+     mocktruck >> Edge(color='magenta', style='solid', xlabel='storeFood', fontcolor='magenta') >> coldstorageservice
+     mocktruck >> Edge(color='magenta', style='solid', xlabel='sendTicket', fontcolor='magenta') >> coldstorageservice
+     mocktruck >> Edge(color='magenta', style='solid', xlabel='deposit', fontcolor='magenta') >> coldstorageservice
+diag
