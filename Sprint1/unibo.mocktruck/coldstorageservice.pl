d6fb8a7c642b9629415c78b115e7eef17c4d3c7f
Leonardo Focardi <leonardo.foc.lav@gmail.com>
2023-08-29 10:28:15 +0200

    userdoc corretti, qak corretti, copiate qakicons

--- Sprint1/unibo.mocktruck/coldstorageservice.pl
+++
@@ -1,6 +0,0 @@
-%====================================================================================
-% coldstorageservice description   
-%====================================================================================
-context(ctxtruck, "localhost",  "TCP", "8092").
- qactor( coldstorageservice, ctxtruck, "it.unibo.coldstorageservice.Coldstorageservice").
-  qactor( mocktruck, ctxtruck, "it.unibo.mocktruck.Mocktruck").
