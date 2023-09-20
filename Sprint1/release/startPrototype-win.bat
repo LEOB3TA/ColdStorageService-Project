a855e2fa6e70ce22ec671a1f3b6deeb4a6a407ad
cronoimpius <gianmiriano.porrazzo@studio.unibo.it>
2023-09-20 15:54:36 +0200

    push modifica bat per release

--- Sprint1/release/startPrototype-win.bat
+++ Sprint1/release/startPrototype-win.bat
@@ -1,24 +1,19 @@
 @echo off
-echo HIT CTRL+C TO CLOSE THE PROGRAM
 
-REM Define a function to gracefully shut down the Docker container
-:cleanup
-docker-compose -f webbasicrobot23.yaml down
-exit /b 0
-
-REM Set up a trap to catch the Ctrl+C signal and run the cleanup function
-echo Waiting for containers to initialize...
+REM Start the Docker container using Docker Compose
 docker-compose -f webbasicrobot23.yaml up -d
-ping 127.0.0.1 -n 5 > nul
 
-REM Open the default web browser to localhost:8090
-start http://localhost:8090
-ping 127.0.0.1 -n 5 > nul
+REM Wait for a few seconds to ensure the container is up and running
+timeout /t 5
 
-REM Start the unibo.prototipo0-1.0-all.jar file (assuming it's in the current directory)
-java -jar unibo.prototipo0-1.0-all.jar
+REM Open the default browser to localhost:8090
+start "" "http://localhost:8090"
 
-REM Keep the script running in the background
-:loop
-ping 127.0.0.1 -n 3600 > nul
-goto :loop
+REM Start the unibo.prototipo0-1.0-all.jar file in the background
+start "" java -jar unibo.prototipo0-1.0-all.jar
+
+REM Wait for the user to press Ctrl+C to stop the container
+pause
+
+REM Stop the Docker container gracefully
+docker-compose -f webbasicrobot23.yaml down
