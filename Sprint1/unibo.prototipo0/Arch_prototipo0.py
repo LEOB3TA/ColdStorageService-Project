8affd815e3c68e33b86e115b5f75bb53f2726e0d
cronoimpius <gianmiriano.porrazzo@studio.unibo.it>
2023-09-29 10:27:52 +0200

    Correzione dettagli

--- Sprint1/unibo.prototipo0/Arch_prototipo0.py
+++ Sprint1/unibo.prototipo0/Arch_prototipo0.py
@@ -1,3 +1,4 @@
+### conda install diagrams
 from diagrams import Cluster, Diagram, Edge
 from diagrams.custom import Custom
 import os
@@ -19,22 +20,17 @@ eventedgeattr = {
 with Diagram('prototipo0Arch', show=False, outformat='png', graph_attr=graphattr) as diag:
   with Cluster('env'):
      sys = Custom('','./qakicons/system.png')
+### see https://renenyffenegger.ch/notes/tools/Graphviz/attributes/label/HTML-like/index
      with Cluster('ctxbasicrobot', graph_attr=nodeattr):
           basicrobot=Custom('basicrobot(ext)','./qakicons/externalQActor.png')
      with Cluster('ctxprototipo0', graph_attr=nodeattr):
           mocktruck=Custom('mocktruck','./qakicons/symActorSmall.png')
           coldstorageservice=Custom('coldstorageservice','./qakicons/symActorSmall.png')
           transporttrolley=Custom('transporttrolley','./qakicons/symActorSmall.png')
-     mocktruck >> Edge(color='magenta', style='solid', xlabel='storeFood', fontcolor='magenta') >> coldstorageservice
-     mocktruck >> Edge(color='magenta', style='solid', xlabel='sendTicket', fontcolor='magenta') >> coldstorageservice
-     mocktruck >> Edge(color='magenta', style='solid', xlabel='deposit', fontcolor='magenta') >> coldstorageservice
-     sys >> Edge(color='red', style='dashed', xlabel='local_movef', fontcolor='red') >> coldstorageservice
-     coldstorageservice >> Edge(color='magenta', style='solid', xlabel='pickup', fontcolor='magenta') >> transporttrolley
-     coldstorageservice >> Edge( xlabel='local_movef', **eventedgeattr, fontcolor='red') >> sys
-     transporttrolley >> Edge(color='magenta', style='solid', xlabel='engage', fontcolor='magenta') >> basicrobot
-     transporttrolley >> Edge(color='blue', style='solid', xlabel='disengage', fontcolor='blue') >> basicrobot
-     transporttrolley >> Edge(color='blue', style='solid', xlabel='setrobotstate', fontcolor='blue') >> basicrobot
-     transporttrolley >> Edge(color='magenta', style='solid', xlabel='moverobot', fontcolor='magenta') >> basicrobot
-     transporttrolley >> Edge(color='blue', style='solid', xlabel='cmd', fontcolor='blue') >> basicrobot
-     transporttrolley >> Edge( xlabel='local_movef', **eventedgeattr, fontcolor='red') >> sys
+     coldstorageservice >> Edge( label='local_movef', **eventedgeattr, fontcolor='red') >> sys
+     transporttrolley >> Edge( label='local_movef', **eventedgeattr, fontcolor='red') >> sys
+     coldstorageservice >> Edge(color='magenta', style='solid', decorate='true', label='<pickup &nbsp; >',  fontcolor='magenta') >> transporttrolley
+     mocktruck >> Edge(color='magenta', style='solid', decorate='true', label='<storeFood &nbsp; sendTicket &nbsp; deposit &nbsp; >',  fontcolor='magenta') >> coldstorageservice
+     transporttrolley >> Edge(color='magenta', style='solid', decorate='true', label='<engage &nbsp; moverobot &nbsp; >',  fontcolor='magenta') >> basicrobot
+     transporttrolley >> Edge(color='blue', style='solid',  label='<disengage &nbsp; setrobotstate &nbsp; cmd &nbsp; >',  fontcolor='blue') >> basicrobot
 diag
--- Sprint1/unibo.prototipo0/prototipo0.pl
+++ Sprint1/unibo.prototipo0/prototipo0.pl
@@ -1,6 +1,19 @@
 %====================================================================================
 % prototipo0 description   
 %====================================================================================
+request( deposit, deposit(_) ).
+request( sendTicket, sendTicket(TICKETID) ).
+request( storeFood, storeFood(FW) ).
+request( pickup, pickup(_) ).
+request( engage, engage(ARG) ).
+dispatch( disengage, disengage(ARG) ).
+request( doplan, doplan(PATH,OWNER,STEPTIME) ).
+request( moverobot, moverobot(TARGETX,TARGETY) ).
+dispatch( setrobotstate, setpos(X,Y,D) ).
+dispatch( setdirection, dir(D) ).
+dispatch( cmd, cmd(MOVE) ).
+event( local_movef, movef(_) ).
+%====================================================================================
 context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
 context(ctxprototipo0, "localhost",  "TCP", "8099").
  qactor( basicrobot, ctxbasicrobot, "external").
