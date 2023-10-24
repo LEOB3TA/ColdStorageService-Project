1a008f9131247286f980e060d27e288a412613c7
leo <leonardo.foc.lav@gmail.com>
2023-10-24 11:49:52 +0200

    first commit for sprint2reviwed

---
+++ Sprint2Reviewed/release/startPrototype-mac.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "HIT CTRL+C TO CLOSE THE PROGRAM"
+
+function cleanup() {
+    # Stop the Docker container
+    docker-compose -f webbasicrobot23.yaml down
+    exit 0
+}
+
+# Set up a trap to catch the Ctrl+C signal and run the cleanup function
+trap cleanup SIGINT
+
+# Start the Docker containers using Docker Compose
+docker-compose -f webbasicrobot23.yaml up -d
+
+# Wait for the containers to initialize
+echo "wait for the containers to inizialize"
+sleep 3
+
+# Open the default web browser to localhost:8090
+open http://localhost:8090
+sleep 15
+
+# Start the unibo.prototipo1-1.0-all.jar file (assuming it's in the current directory)
+java -jar unibo.prototipo1-1.0-all.jar
+
+
+# Keep the script running in the background
+while true; do
+    sleep 1
+done
---
+++ Sprint2Reviewed/unibo.prototipo1/src/rx/sonarSimulator.kt
@@ -0,0 +1,69 @@
+package rx
+
+import it.unibo.kactor.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import unibo.basicomm23.interfaces.IApplMessage
+import unibo.basicomm23.utils.CommUtils
+import kotlin.random.Random
+
+/*
+-------------------------------------------------------------------------------------------------
+sonarSimulator.kt
+-------------------------------------------------------------------------------------------------
+ */
+
+class sonarSimulator ( name : String ) : ActorBasic( name ) {
+	init{
+		//autostart
+
+		runBlocking{  autoMsg("simulatorstart","do") }
+	}
+//@kotlinx.coroutines.ObsoleteCoroutinesApi
+
+    override suspend fun actorBody(msg : IApplMessage){
+  		println("$tt $name | received  $msg "  )
+		delay(10000)
+		if( msg.msgId() == "simulatorstart") startDataReadSimulation(   )
+     }
+  	
+//@kotlinx.coroutines.ObsoleteCoroutinesApi
+
+	suspend fun startDataReadSimulation(    ){
+		/*val data = sequence<Int>{
+			var v0 = 80
+			yield(v0)
+			while(true){
+				v0 = v0 - 5
+				yield( v0 )
+			}
+		}*/
+		var i = 0
+			while( i < 20 ){
+ 	 			//val m1 = "distance( ${data.elementAt(i)} )"
+				val m1= "distance( ${Random.nextInt(0,80)} )"
+				i++
+ 				val event = CommUtils.buildEvent( name,"sonardistance", m1)
+  				//emitLocalStreamEvent( event ) //ORIGINAL FROM NATALI
+ 				println("$tt $name | generates $event")
+ 				emit(event)  //APPROPRIATE ONLY IF NOT INCLUDED IN A PIPE
+ 				delay( 1500 )
+  			}
+			emit(CommUtils.buildEvent( name,"sonardistance", "distance(35)"))
+			terminate()
+	}
+
+} 
+
+//@kotlinx.coroutines.ObsoleteCoroutinesApi
+//
+//fun main() = runBlocking{
+// //	val startMsg = MsgUtil.buildDispatch("main","start","start","datasimulator")
+//	val consumer  = dataConsumer("dataconsumer")
+//	val simulator = sonarSimulator( "datasimulator" )
+//	val filter    = dataFilter("datafilter", consumer)
+//	val logger    = dataLogger("logger")
+//	simulator.subscribe( logger ).subscribe( filter ).subscribe( consumer ) 
+//	MsgUtil.sendMsg("start","start",simulator)
+//	simulator.waitTermination()
+// } 
