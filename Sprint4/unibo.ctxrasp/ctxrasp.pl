cad610ff9dcc1641f7db134c7eae2387e173ea63
leo <leonardo.foc.lav@gmail.com>
2023-11-15 07:48:54 +0100

    fix unused messages

--- Sprint4/unibo.ctxrasp/ctxrasp.pl
+++ Sprint4/unibo.ctxrasp/ctxrasp.pl
@@ -1,27 +1,12 @@
 %====================================================================================
 % ctxrasp description   
 %====================================================================================
-request( engage, engage(ARG) ).
-reply( engagedone, engagedone(ARG) ).  %%for engage
-reply( engagerefused, engagerefused(ARG) ).  %%for engage
-dispatch( disengage, disengage(ARG) ).
-request( doplan, doplan(PATH,OWNER,STEPTIME) ).
-reply( doplandone, doplandone(ARG) ).  %%for doplan
-reply( doplanfailed, doplanfailed(ARG) ).  %%for doplan
-request( moverobot, moverobot(TARGETX,TARGETY) ).
-reply( moverobotdone, moverobotok(ARG) ).  %%for moverobot
-reply( moverobotfailed, moverobotfailed(PLANDONE,PLANTODO) ).  %%for moverobot
-dispatch( setrobotstate, setpos(X,Y,D) ).
-dispatch( setdirection, dir(D) ).
-dispatch( cmd, cmd(MOVE) ).
 event( sonardata, distance(D) ).
 event( alarm, alarm(X) ).
 event( stop, stop(_) ).
 event( resume, resume(_) ).
 dispatch( sonaractivate, info(D) ).
 dispatch( ledCmd, ledCmd(CMD) ).
-dispatch( gotoblink, gotoblink(_) ).
-dispatch( coapUpdate, coapUpdate(RES,VAL) ).
 %====================================================================================
 context(ctxrasp, "localhost",  "TCP", "8099").
  qactor( sonar, ctxrasp, "rx.sonarHCSR04Support23").
