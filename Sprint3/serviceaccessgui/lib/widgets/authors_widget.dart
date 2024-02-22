6e969bfa532f23436b43256cfac6cd917cd95224
cronoimpius <gianmiriano.porrazzo@studio.unibo.it>
2024-02-22 14:02:23 +0100

    Correct also serviceaccess XD

--- Sprint3/serviceaccessgui/lib/widgets/authors_widget.dart
+++ Sprint3/serviceaccessgui/lib/widgets/authors_widget.dart
@@ -10,23 +10,27 @@ class AuthorsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //return SizedBox(width: double.maxFinite, child: DecoratedBox(decoration: BoxDecoration(color: Globals.backgroundWidgetColor, borderRadius: BorderRadius.circular(16)), child: const Center(child: Text('Authors', style: TextStyle(fontSize: 24,),)),));
-    child: const Padding(padding: const EdgeInsets.all(16),
-      child: Center(child: SpacedColumn(
-        spacing: 8,
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment:CrossAxisAlignment.center,
-        children: [
-          Text('Authors', style:TextStyle(fontWeight: FontWeight.bold)),
-          SpacedRow(
-            spacing: 8,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment:CrossAxisAlignment.center,
-            children: [
-              Text('Leonardo Focardi'),
-              Text('Christian Galeone'),
-              Text('Gianmiriano Porrazzo')
-            ],
-          ),],),),)
+    return SizedBox(width: double.maxFinite,
+        child: DecoratedBox(decoration: BoxDecoration(color: Globals.backgroundWidgetColor, borderRadius: BorderRadius.circular(16)),
+          //child: const Center(child: Text('Authors', style: TextStyle(fontSize: 24,),))
+          child: const Padding(padding: const EdgeInsets.all(16),
+            child: Center(child: SpacedColumn(
+              spacing: 8,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment:CrossAxisAlignment.center,
+              children: [
+                Text('Authors', style:TextStyle(fontWeight: FontWeight.bold)),
+                SpacedRow(
+                  spacing: 8,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment:CrossAxisAlignment.center,
+                  children: [
+                    Text('Leonardo Focardi'),
+                    Text('Christian Galeone'),
+                    Text('Gianmiriano Porrazzo')
+                  ],
+                ),],),),)
+
           ,));
   }
 }
